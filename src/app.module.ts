54bed1ca83bb5594669ee4f1e78ac54d027cdfb7
Alanlan21 <alanregisrock@gmail.com>
2024-11-05 23:07:18 -0300

    consertado bug do NaN

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,28 +5,41 @@ import { UserModule } from './user/user.module';
 import { MenuItemModule } from './menuItem/menuItem.module';
 import { StoreModule } from './store/store.module';
 import { OwnerModule } from './owner/owner.module';
-import { Order } from './order/order.entity';
 import { OrderModule } from './order/order.module';
-import { PaymentMethodController } from './payment-method/payment-method.controller';
 import { PaymentMethodModule } from './payment-method/payment-method.module';
 import { PaymentModule } from './payment/payment.module';
+import { StripeModule } from './stripe/stripe.module';
+import { PaymentMethodController } from './payment-method/payment-method.controller';
 import { StripeController } from './stripe/stripe.controller';
 import { StripeService } from './stripe/stripe.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { StripeModule } from './stripe/stripe.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'unimenu',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const isMySQL = !!configService.get<string>('DB_HOST');
+        return isMySQL
+          ? {
+              type: 'mysql',
+              host: configService.get<string>(''),
+              port: parseInt(configService.get<string>('DB_PORT'), 10) || 3306,
+              username: configService.get<string>('DB_USERNAME'),
+              password: configService.get<string>('DB_PASSWORD'),
+              database: configService.get<string>('DB_NAME'),
+              autoLoadEntities: true,
+              synchronize: true,
+            }
+          : {
+              type: 'sqlite',
+              database: configService.get<string>('SQLITE_DB_PATH') || ':memory:',
+              autoLoadEntities: true,
+              synchronize: true,
+            };
+      },
     }),
     AuthModule,
     UserModule,
@@ -36,16 +49,14 @@ import { StripeModule } from './stripe/stripe.module';
     OrderModule,
     PaymentMethodModule,
     PaymentModule,
-    StripeModule
-    
-     
+    StripeModule,
   ],
   controllers: [PaymentMethodController, StripeController],
-  providers: [StripeService]
+  providers: [StripeService],
 })
 export class AppModule {
   constructor(private configService: ConfigService) {
     const stripeSecretKey = this.configService.get<string>('STRIPE_SK');
     Logger.log(`Stripe Secret Key: ${stripeSecretKey}`);
-} 
   }
+}
