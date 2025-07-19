57b77e1aa75e58f6ece015b222255f04ba84ad73
Alanlan21 <alanregisrock@gmail.com>
2025-07-19 12:41:42 -0300

    Remove frontend folder from backend code

--- frontend/src/types/stripe.ts
+++
@@ -1,7 +0,0 @@
-import { Stripe, StripeElements, StripeCardElement } from '@stripe/stripe-js';
-
-export interface StripeState {
-  stripe: Stripe | null;
-  elements: StripeElements | null;
-  cardElement: StripeCardElement | null;
-}
